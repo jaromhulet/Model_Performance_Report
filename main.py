e111c2c7166fecf470fc630d4845edc5820c9492
jaromhulet <jarom.hulet@gmail.com>
2020-09-07 21:08:35 -0500

    code updates and continued development

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from pandas.api.types import is_numeric_dtype
 import matplotlib.pyplot as plt
 import math
+from sklearn.metrics import mean_squared_error
 
 
 
@@ -42,12 +43,19 @@ class performanceReport():
             self.df = df_test
         else:
             self.one_df = 0
+            
+        #create residuals
+        if self.one_df == 1:
+            self.df['RESID'] = self.df[self.actual_col_name] - self.df[self.pred_col_name]
+        else:
+            self.df_test['RESID'] = self.df_test[self.actual_col_name] - self.df_test[self.pred_col_name]
+            self.df_train['RESID'] = self.df_train[self.actual_col_name] - self.df_train[self.pred_col_name]
         
        
    
    
-    def perfChart(self,metric_function,metric_name,chart_title,by='Empty',filters=[],bins=5,barwidth=0,barWidth=0.25,color1='#7f6d5f',color2='#557f2d',figsize=[30,8],
-                  x_tick_rotation=45,fig_path='fig1.png'):
+    def perfChart(self,metric_function,metric_name,chart_title,by='Empty',filters=[],bins=5,barwidth=0,barWidth=0.25,trainColor='#7f6d5f',testColor='#557f2d',figsize=[30,8],
+                  x_tick_rotation=45,fig_path='fig1.png',all_metrics=True):
         
         
         #first, condition on if there are two datasets
@@ -126,13 +134,21 @@ class performanceReport():
                 raise dftrain_dftest_type_mismatch
             
             
-            
             #loop through all buckets and calculate performance for each bucket
-            for j in unique_vals:
-                
+            train_perf_all = metric_function(df_train_temp[self.pred_col_name],df_train_temp[self.actual_col_name])
+            test_perf_all = metric_function(df_test_temp[self.pred_col_name],df_test_temp[self.actual_col_name])                                
+            
+            if all_metrics:
+                train_perf = [train_perf_all]
+                test_perf = [test_perf_all]
+                label_list = ['All']
+            else:
                 train_perf = []
                 test_perf = []
-                label_list = []
+                label_list = []                
+                
+            
+            for j in unique_vals:
             
                 train_perf_all = metric_function(df_train_temp[self.pred_col_name],df_train_temp[self.actual_col_name])
                 test_perf_all = metric_function(df_test_temp[self.pred_col_name],df_test_temp[self.actual_col_name])                
@@ -156,16 +172,19 @@ class performanceReport():
                 test_perf.append(temp_test_perf)
                 
                 label_list.append(j)
+                
+            
             
             #set position of bar on X-axis    
             r1 = np.arange(len(label_list))
             r2 = [x + barWidth for x in r1]
-            r3 = [x + barWidth for x in r2]
+
             
-            plt.bar(r1,train_perf, color=color1, width=barWidth,edgecolor='white',label=('Train %s'%metric_name))
-            plt.bar(r2,test_perf, color=color2, width=barWidth,edgecolor='white',label=('Test %s'%metric_name))
+            plt.bar(r1,train_perf, color=trainColor, width=barWidth,edgecolor='white',label=('Train %s'%metric_name))
+            plt.bar(r2,test_perf, color=testColor, width=barWidth,edgecolor='white',label=('Test %s'%metric_name))
             
             plt.xlabel(by, fontweight='bold')
+            #plt.xticks([r + barWidth for r in range(len(test_perf))],label_list)
             plt.xticks([r + barWidth for r in range(len(test_perf))],label_list)
             plt.ylabel(metric_name, fontweight='bold')
             
@@ -183,6 +202,115 @@ class performanceReport():
         else:
             print('There is only one dataframe provided - I need to make the code to handle this')
             
+            #the filters variable must be a list of list. Each sublist must have the following three elements
+            #1, the name of the column to be filtered
+            #2, 'lt', 'lte', 'gt', 'gte' , 'e' and 'ne' to designate the type of equality/inequality
+            #3, the value to filter by
+            #for example filters = [['col1_name','lte','6'],[col2_name','e','VALUE1']]
+            if len(filters)!=0:
+                for i in filters:
+                    if i[1] == 'lt':
+                        df_temp = self.df[self.df[i[0]]<i[2]]
+                                                    
+                    elif i[1] == 'lte':
+                        df_temp = self.df[self.df[i[0]]<=i[2]]              
+                        
+                    elif i[1] == 'gt':
+                        df_temp = self.df[self.df[i[0]]>i[2]]                   
+                        
+                    elif i[1] == 'gte':
+                        df_temp = self.df[self.df[i[0]]>=i[2]]                   
+                        
+                    elif i[1] == 'e':
+                        df_temp = self.df[self.df[i[0]]==i[2]]
+                        
+                    elif i[1] == 'ne':
+                        df_temp = self.df[self.df[i[0]]!=i[2]]
+
+            else:
+                df_temp = self.df
+                    
+            
+            #if data is numeric cut into bins and calculate
+            if is_numeric_dtype(self.df[by]):
+                
+                #binning logic
+                temp_max = df_temp[by].max()
+                temp_min =df_temp[by].min()
+                step_size = (temp_max - temp_min)/(bins-1)
+                
+                bin_list = [temp_min]
+                
+                for i in range(0,bins):
+                    temp_min = temp_min + step_size
+                    bin_list.append(temp_min)
+                    
+                temp_col_name = by + '_grouped'
+                
+                df_temp[temp_col_name] = pd.cut(df_temp[by],bin_list)
+                    
+                
+            elif not is_numeric_dtype(self.df[by]):
+                
+                unique_vals = df_temp[by].sort_values().unique() 
+                
+                temp_col_name = by + '_grouped'
+                
+                df_temp[temp_col_name] = df_temp[by]
+                
+                
+            else:
+                raise dftrain_dftest_type_mismatch
+            
+            
+            #loop through all buckets and calculate performance for each bucket
+            perf_all = metric_function(df_temp[self.pred_col_name],df_temp[self.actual_col_name])
+            
+            if all_metrics:
+                perfs = [perf_all]
+                label_list = ['All']
+            else:
+                perfs = []
+                label_list = []                
+                
+            
+            for j in unique_vals:
+                
+                df_temp2 = df_temp[df_temp[temp_col_name]==j]
+
+                
+                if len(df_temp2) == 0:
+                    temp_perf = 0
+                else:
+                    temp_perf = metric_function(df_temp2[self.pred_col_name],df_temp2[self.actual_col_name])
+                    
+                    
+                #append metrics to list
+                perfs.append(temp_perf)
+                label_list.append(j)
+                
+            
+            #set position of bar on X-axis    
+            r1 = np.arange(len(label_list))
+            r2 = [x + barWidth for x in r1]
+            
+            plt.bar(r1,perfs, color=trainColor, width=barWidth,edgecolor='white',label=('Train %s'%metric_name))
+            
+            plt.xlabel(by, fontweight='bold')
+            #plt.xticks([r + barWidth for r in range(len(perfs))],label_list)
+            plt.ylabel(metric_name, fontweight='bold')
+            
+            plt.rcParams["figure.figsize"] = (figsize[0],figsize[1])
+            
+            plt.title(chart_title)
+            plt.xticks(rotation=x_tick_rotation,ha='right')
+            
+            plt.legend()
+            
+            plt.savefig(fig_path)      
+            
+            plt.show()            
+            
 
 #create test data to run through code
 test_df = pd.read_csv('sample_data.csv')
@@ -195,15 +323,15 @@ train_df['PRED']=train_df['PRED'].astype(float)
 train_df['ACTUAL'] = train_df['ACTUAL'].astype(float)
 
 def rmse(y,yhat):
-    temp_dif = y - yhat
-    return temp_dif.mean()
+    return math.sqrt(mean_squared_error(y,yhat))
 
 
-test_report = performanceReport('PRED','ACTUAL',df_test= test_df, df_train=train_df)     
+test_report = performanceReport('PRED','ACTUAL',df_test= test_df,df_train=train_df)     
 
 #print(test_df.head(10))
 
 test_report.perfChart(metric_function=rmse,metric_name='RMSE',chart_title='RMSE by Group1',by='GROUP_1')
+
    
     #def perfChart(self,metric_function,metric_name,chart_title,by='Empty',filters=[],bins=5,barwidth=0,barWidth=0.25,color1='#7f6d5f',color2='#557f2d',figsize=[30,8],
      #             x_tick_rotation=45,fig_path='fig1.png'):
